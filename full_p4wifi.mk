72dbdaa2b2e656d37e1ea491f83a388c26113de7
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-05-14 20:54:37 +0200

    Update to PA

--- full_p4wifi.mk
+++ full_p4wifi.mk
@@ -1,13 +1,10 @@
 # Inherit device configuration for p4wifi.
 $(call inherit-product, device/samsung/p4wifi/p4wifi.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_p4wifi
+PRODUCT_NAME := pa_p4wifi
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := p4wifi
 PRODUCT_MODEL := GT-P7510
