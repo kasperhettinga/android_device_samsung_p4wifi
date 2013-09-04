b80896def4984063c333c9f23bbd905e2f4f66c8
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-09-04 07:19:05 +0200

    Back to CM build script for PAC

--- cm.mk
+++ cm.mk
@@ -2,12 +2,12 @@
 $(call inherit-product, device/samsung/p4wifi/p4wifi.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/carbon/config/common_tablet.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := carbon_p4wifi
+PRODUCT_NAME := cm_p4wifi
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := p4wifi
 PRODUCT_MODEL := GT-P7510
