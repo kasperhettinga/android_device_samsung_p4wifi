515b785075eb8ca3aaaac241f9a903c3bc63584e
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-10-17 21:22:44 +0200

    Custom -> OMNI

--- omni_p4wifi.mk
+++ omni_p4wifi.mk
@@ -2,12 +2,12 @@
 $(call inherit-product, device/samsung/p4wifi/p4wifi.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/custom/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := custom_p4wifi
+PRODUCT_NAME := omni_p4wifi
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := p4wifi
 PRODUCT_MODEL := GT-P7510
