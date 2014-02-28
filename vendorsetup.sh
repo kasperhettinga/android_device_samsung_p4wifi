249469ce1906a93743e30dd0188c405ec07c3f42
kasperhettinga <kasper.hettinga@gmail.com>
2014-02-28 20:05:14 +0100

    Add patches for tegra
    Based on the p990 device tree of hjunii

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,68 @@
 # limitations under the License.
 #
 
-add_lunch_combo omni_p4wifi-userdebug
+add_lunch_combo nameless_p4wifi-userdebug
+
+echo ""
+echo "Applying patches for p4wifi"
+echo "Based on patches for p990 from hjunii"
+echo ""
+
+echo "Apply patch to frameworks/native"
+echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
+(cd frameworks/native; git am ../../device/samsung/p4wifi/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+    echo "     [DONE]"
+else
+    (cd frameworks/native; git am --abort)
+    echo "     [FAIL]"
+fi
+
+echo "Apply patch to frameworks/av"
+echo -n "Apply patch 0001-ifdef-for-ICS-Audio-Blob-compatibility.patch"
+(cd frameworks/av; git am ../../device/samsung/p4wifi/patches/0001-ifdef-for-ICS-Audio-Blob-compatibility.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
+(cd frameworks/av; git am ../../device/samsung/p4wifi/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to external/skia"
+echo -n "Apply patch 0001-external-skia-patch.patch"
+(cd external/skia; git am ../../device/samsung/p4wifi/patches/0001-external-skia-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd external/skia; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to external/chromium_org"
+echo -n "Apply patch 0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
+(cd external/chromium_org; git am ../../device/samsung/p4wifi/patches/0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd external/chromium_org; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to hardware/libhardware"
+echo -n "Apply patch 0001-Star-audio-patch.patch"
+(cd hardware/libhardware; git am ../../device/samsung/p4wifi/patches/0001-Star-audio-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd hardware/libhardware; git am --abort)
+	echo "     [FAIL]"
+fi
